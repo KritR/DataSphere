c9a0d4699b4ce0a47bce3220722f24fc6c6da97c
Ryan Luu <rl65536@gmail.com>
2020-01-25 11:54:02 -0500

    Prettify UI

--- frontend/index.js
+++ frontend/index.js
@@ -1,21 +1,24 @@
 var earth;
-var curMarkers = []; 
+var curMarkers = [];
 
 function initialize() {
-      const options = {atmosphere: true, center: [0, 0], zoom: 0};
-      earth = new WE.map('earth_div', options);
-      WE.tileLayer('http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', {
-      minZoom: 0,
-      maxZoom: 5,
-      attribution: 'NASA'
+    const options = { atmosphere: true, center: [0, 0], zoom: 0 };
+    earth = new WE.map('earth_div', options);
+    WE.tileLayer('http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', {
+        minZoom: 0,
+        maxZoom: 5,
+        attribution: 'NASA'
     }).addTo(earth);
 
 }
 
-function change_year() {
+function change_year_label() {
     yearLabel = document.getElementById("yearLabel")
     slider = document.getElementById("myRange")
     yearLabel.innerHTML = parseInt(slider.value) + 1920
+}
+
+function fetch_markers() {
     for (const marker of curMarkers) {
         marker.detach()
     }
@@ -29,7 +32,7 @@ function fetch(year) {
         console.log(res.data.data)
         for (const event of res.data.data) {
             var marker = WE.marker([event.location.lat, event.location.lon]).addTo(earth);
-            marker.bindPopup(`<b>${event.title}</b><br>${event.description}<br/><span style='font-size:10px;color:#999'></span>`, {maxWidth: 150, closeButton: true});
+            marker.bindPopup(`<b>${event.title}</b><br>${event.description}<br/><span style='font-size:10px;color:#999'></span>`, { maxWidth: 150, closeButton: true });
             curMarkers.push(marker)
         }
     }).catch(err => {
