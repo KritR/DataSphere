426483fb423a5f352158f035a6756437944b3777
Krithik Rao <krdevmail@gmail.com>
2020-01-26 02:34:33 -0500

    Added Proper IP

--- backend/config/prod.exs
+++ backend/config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :wikiglobe, WikiglobeWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "104.154.113.20", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :cors_plug,
