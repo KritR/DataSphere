aa66ba1344ea6c83a7cd274da82d30f3442aab90
Anderson <andersondavid2000@gmail.com>
2020-01-26 04:51:15 -0500

    https

--- frontend/index.js
+++ frontend/index.js
@@ -9,7 +9,7 @@ var month;
 function initialize() {
     const options = { atmosphere: true, center: [0, 0], zoom: 0 };
     earth = new WE.map('earth_div', options);
-    WE.tileLayer('http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', {
+    WE.tileLayer('https://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', {
         minZoom: 0,
         maxZoom: 5,
         attribution: 'NASA'
